c8c5e0f343410486b9a635e9b63b48394f7a8375
yuvrajchaudhari <yuvrajchaudhari011@gmail.com>
2025-05-09 22:15:26 +0530

    04-string assignment
    04-string assignment

---
+++ 04-string.js
@@ -0,0 +1,48 @@
+var greet = "Good Morning";
+console.log("====================Find the length==================");
+var greetLength = greet.length;
+console.log(greet , "String Length is :",  greetLength);
+console.log("===================string template===============");
+console.log(`${greet} String Length is, ${greetLength}`);
+
+console.log("========== chatAt() ===============");
+var chatAtIndex0 = greet.charAt(0);
+console.log(`Char available at index 0 is: ${chatAtIndex0}`);
+
+var charAtIndex7 = greet.charAt(7);
+console.log(`Char available at index 7 is: ${charAtIndex7}`);
+
+var charAtIndex50 = greet.charAt(50);
+console.log(`Char available at index 50 is: ${charAtIndex50}`);
+
+var str = "Hello buddy how are you doing";
+var charAtLastIndex = str.charAt(str.length-1);
+console.log(`Char available at last index is: ${charAtLastIndex}`);
+
+
+console.log("========== concat() ===============");
+var firstName = "Stew";
+var lastName = " Jobs";
+var result = firstName + lastName;
+console.log(`Concat strings using + operator ${result} `);
+
+var result = firstName.concat(lastName).concat(" I am unique");
+console.log(`Concat strings using concat():  ${result} `);
+
+console.log("========== indexOf() ===============");
+var greet = "Good Morning";
+var indexOfO = greet.indexOf("o");
+console.log(`Index of char o : ${indexOfO} `);
+var indexOfZ = greet.indexOf("z");
+console.log(`Index of char z : ${indexOfZ} `);
+
+var indexOfMorning = greet.indexOf("Morning");
+console.log(`Index of sub string "Morning" : ${indexOfMorning} `);
+
+console.log("========== trim() ===============");
+var day = "  Today is Friday   ";
+console.log(`Before trim: ${day.length}`);
+var trimResult = day.trim();
+console.log(`After trim: ${trimResult.length}`);
+var totalRemovedSpaces = day.length - trimResult.length;
+console.log(`Total Spaces removed after trim() is: ${totalRemovedSpaces}`);
---
+++ o4-string-assignment.js
@@ -0,0 +1,18 @@
+function stringBasics(){
+    console.log(`=========assignment 1===========`)
+    var dreamCompany = "Accenture"
+    console.log(`My dream company is : ${dreamCompany}`);
+
+    console.log(`=========assignment 2===========`)
+    var hobbyOne = "Reading";
+    var hobbyTwo = "Gardning";
+    var hobbyThree = "Coding"
+    console.log(`My Hobbies are : ${hobbyOne}, ${hobbyTwo}, ${hobbyThree}`);
+    
+    var totalLength = hobbyOne.length + hobbyTwo.length + hobbyThree.length;
+    console.log(`sum of total no of hobbies character is: ${totalLength}`);
+    
+    
+}
+
+stringBasics()
